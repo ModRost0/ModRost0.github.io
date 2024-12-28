d7e086d0e07f417d87f998c9288b3a2843004e58
unknown <sheikhanas2019@gmail.com>
2024-12-28 23:32:52 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -56,6 +56,7 @@ app.use(cors({
 }));
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(session(sessionConfig));
 app.use(passport.initialize());
