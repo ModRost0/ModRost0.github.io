4e28632e27318241a14ff03fd05460c25aeb6b2d
ModRost <143633269+ModRost0@users.noreply.github.com>
2024-12-31 21:57:46 +0530

    Updated Video

--- chatApp/src/AppWrapper.jsx
+++ chatApp/src/AppWrapper.jsx
@@ -9,16 +9,13 @@ import App from './App';
 import ProtectedRoute from './components/ProtectedRoute';
 import './AppWrapper.css';
 import { UserContext } from './context/UserContext.jsx';
-
+import VideoComponent from './VideoComponent'
 const AppWrapper = () => {
   let {user} = useContext(UserContext)
   return (
     <div className="app-wrapper">
       {/* Video Background */}
-      <video autoPlay loop className="cloudinary-vid">
-        <source src="https://res.cloudinary.com/dritmjkxl/video/upload/v1735657604/eldenRingCinematics_-_Made_with_Clipchamp_1_lrqidl.mp4" type="video/mp4" />
-        Your browser does not Support the video tag.
-      </video>
+      <VideoComponent />
       <NavBar />
       <Container maxWidth="lg" sx={{ paddingTop: 4, paddingBottom: 4 }}>
         <Box sx={{ paddingTop: 4, paddingBottom: 4, textAlign: 'center' }}>
